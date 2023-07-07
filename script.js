53979bf3cc26d03fb11a6dec2a33095d4eb7c477
ehliexplore <lgspotifyemail@gmail.com>
2023-07-07 04:20:59 -0300

    config button added
    Foi adicionado o botão "configurações", que quando clicado, limpa o conteúdo do <main> e mostra opções de configurações, onde é possível alterar o tema do site e retornar para a tela principal.

--- script.js
+++ script.js
@@ -89,3 +89,73 @@ const buttons = document.querySelectorAll('button');
 buttons.forEach((button) => {
   button.addEventListener('click', playSound);
 });
+
+
+// config
+
+function showConfig() {
+  cleanMain();
+
+  const backButton = document.querySelector('.js-back').innerHTML = '<button onclick="backMain()">Back</button>';
+
+  const configContent = document.querySelector('.js-config').innerHTML = `<button onclick="changeTheme()">change theme</button>`;
+
+};
+
+function cleanMain() {
+  const layoutContent = document.querySelector('.js-main').innerHTML = '';
+};
+
+function changeTheme() {
+  if (document.body.style.backgroundColor === 'lightgray') {
+    document.body.style.backgroundColor = 'black';
+    
+  } else {
+    document.body.style.backgroundColor = 'lightgray';
+    
+  }
+};
+
+function backMain() { 
+  document.querySelector('.js-config').innerHTML = '';
+  document.querySelector('.js-back').innerHTML = '';
+
+  document.querySelector('.js-main').innerHTML = `
+  <main class="main-style js-main">
+  <button class="config-button" onclick="showConfig()">Configurações</button>
+
+  <div class="pads-div-1">
+    <button id="kickButton" class="pad-style-1"></button>
+    <button id="snareButton" class="pad-style-2"></button>
+    <button id="hatButton" class="pad-style-3"></button>
+    <button id="openhatButton" class="pad-style-4"></button>
+  </div>
+  <div class="pads-div-2">
+    <button id="button808" class="pad-style-5"></button>
+    <button id="clapButton" class="pad-style-6"></button>
+    <button id="congaButton" class="pad-style-7"></button>
+    <button id="metalshakeButton" class="pad-style-8"></button>
+  </div>
+
+  <div class="melody-div">
+    <button id="doButton" class="melody-button-style-first"></button>
+    <button id="reButton" class="melody-button-style-second"></button>
+    <button id="miButton" class="melody-button-style"></button>
+    <button id="faButton" class="melody-button-style-center"></button>
+    <button id="solButton" class="melody-button-style"></button>
+    <button id="laButton" class="melody-button-style-penultimate"></button>
+    <button id="siButton" class="melody-button-style-last"></button>
+  </div>
+</main>
+
+<div class="js-back"></div>
+<div class="config-content js-config"></div>
+  `;
+
+  // Reattach event listeners to the buttons
+  const buttons = document.querySelectorAll('button');
+  buttons.forEach((button) => {
+    button.addEventListener('click', playSound);
+  });
+}
+
